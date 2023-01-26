b4f9134b25c2e175d3caf47a7d41a0d4c7c66a8c
Jeff <jsford64@gmail.com>
2023-01-26 13:02:00 -0500

    Fixed arm64 include problems

--- src/varintdecode.c
+++ src/varintdecode.c
@@ -11,8 +11,8 @@
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
      /* GCC-compatible compiler, targeting x86/x86-64 */
      #include <x86intrin.h>
-#elif defined(__GNUC__) && defined(__ARM_NEON__)
-     /* GCC-compatible compiler, targeting ARM with NEON */
+#elif defined(__ARM_NEON__)
+     /* targeting ARM with NEON */
      #include <arm_neon.h>
      #include "sse2neon.h"
 #elif defined(__GNUC__) && defined(__IWMMXT__)
